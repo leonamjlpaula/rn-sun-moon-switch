a028350a9003a433d9ae64dd0fdf6b9126680fde
Leonam de Paula <leonamjlpaula@gmail.com>
2025-11-02 10:58:28 -0300

    updated expo and RN + removed storybook

--- App.tsx
+++ App.tsx
@@ -1,1 +1,22 @@
-export { default } from './storybook';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { SunMoonSwitch } from './src/components';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <SunMoonSwitch />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
